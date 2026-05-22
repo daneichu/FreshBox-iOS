23d1e406118ccdfd37222afeb9bfdbfc2cc38e33
김가현 <danchukim2@naver.com>
2026-05-22 18:47:46 +0900

    Initial Commit

---
+++ FreshBox/FreshBoxApp.swift
@@ -0,0 +1,22 @@
+//
+//  FreshBoxApp.swift
+//  FreshBox
+//
+//  Created by kimgahyun on 5/19/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main›
+struct FreshBoxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
+ 
